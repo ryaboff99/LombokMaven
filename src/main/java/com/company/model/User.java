01700ac675f283182602062367af3c3bd9554c52
Oleksii Riabov <ryaboff99@gmail.com>
2022-01-12 20:01:04 +0200

    Update User.java
    * Add Lombok annotations

--- src/main/java/com/company/model/User.java
+++ src/main/java/com/company/model/User.java
@@ -1,5 +1,11 @@
 package com.company.model;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
     private String firstName;
     private String lastName;
